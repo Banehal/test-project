296191eda0f5691b993bb73891a30f32abb92fbb
Banehal <banehallovvv1@gmail.com>
2019-10-18 16:02:42 +0300

    Factorial

---
+++ facto.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main() 
+{
+    int result{1};
+    int n;
+
+    std::cout << "Enter a number" << std::endl;
+    std::cin >> n;
+
+      for (int i=1; i<=n; i=i+1) 
+      {
+          result = result * i;
+      }
+         std::cout << result << std::endl;
+}
+     
