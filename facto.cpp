a0726e26dbb061ec9e97c90370b64e0caf7965b0
Banehal <banehallovvv1@gmail.com>
2019-10-18 17:35:42 +0300

    Factorial edit 3 (if, else).

--- facto.cpp
+++ facto.cpp
@@ -8,10 +8,19 @@ int main()
     std::cout << "Enter a number" << std::endl;
     std::cin >> n;
 
+    if (n<0) 
+    {
+      std::cout << "Enter a positive number" << std::endl;
+    }
+     else 
+    
+    { 
+      
       for (int i=1; i<=n; i=i+1) 
       {
           result = result * i;
       }
          std::cout << n << "! = " << result << std::endl;
+    }    
 }
      
