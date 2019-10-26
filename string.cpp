2a0bc9155b181c3651d6b5dd150173c610d59e25
Banehal <banehallovvv1@gmail.com>
2019-10-26 12:16:39 +0300

    Output of two words.

---
+++ string.cpp
@@ -0,0 +1,15 @@
+ #include <iostream>
+ #include <string>
+
+ int main()
+ {
+    std::string wordOne;
+    std::cout << "Enter first word:" << std::endl;
+    std::cin >> wordOne;
+
+    std::string wordTwo;
+    std::cout << "Enter second word" << std::endl;
+    std::cin >> wordTwo;
+
+    std::cout << "These words are: " << wordOne << " and " << wordTwo << "." << std::endl;
+ }
