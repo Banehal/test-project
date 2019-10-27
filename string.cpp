37c445cad72e90bbad5f2caefa2bf23f4c752d59
Banehal <banehallovvv1@gmail.com>
2019-10-27 16:28:10 +0200

    String length and characters.

--- string.cpp
+++ string.cpp
@@ -7,9 +7,18 @@
     std::cout << "Enter first word:" << std::endl;
     std::cin >> wordOne;
 
-    std::string wordTwo;
-    std::cout << "Enter second word" << std::endl;
-    std::cin >> wordTwo;
+  //  std::string wordTwo;
+  //  std::cout << "Enter second word" << std::endl;
+  //  std::cin >> wordTwo;
 
-    std::cout << "These words are: " << wordOne << " and " << wordTwo << "." << std::endl;
+  //  std::cout << "These words are: " << wordOne << " and " << wordTwo << "." << std::endl;
+  //  std::cout << wordOne.back() << std::endl;
+
+    int wordLength = wordOne.length();
+  //  std::cout << wordOne.length() << std::endl;
+  //  std::cout << wordLength << std::endl;
+      std::cout << wordOne[wordLength-2] << std::endl;
+
+
+
  }
